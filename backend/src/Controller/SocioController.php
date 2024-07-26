f5be8f0732ccbec4d2f84b688e55c12ab270e205
Abrahão Eneias <abrahao.eneias@dce.ufpb.br>
2024-07-25 21:55:10 -0300

    crud de Socios

--- backend/src/Controller/SocioController.php
+++ backend/src/Controller/SocioController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Socio;
+use App\Form\SocioType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +15,39 @@ class SocioController extends AbstractController
     #[Route('/socio', name: 'app_socio')]
     public function index(): Response
     {
-        return $this->render('socio/index.html.twig', [
-            'controller_name' => 'SocioController',
+        return $this->render('socio/index.html.twig');
+    }
+
+    #[Route('/socio/new', name: 'app_socio_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $socio = new Socio();
+        $form = $this->createForm(SocioType::class, $socio);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $now = new \DateTimeImmutable();
+            $socio->setCreatedAt($now);
+            $socio->setUpdatedAt($now);
+
+            $entityManager->persist($socio);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_socio_list');
+        }
+
+        return $this->render('socio/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/socio/list', name: 'app_socio_list')]
+    public function list(EntityManagerInterface $entityManager): Response
+    {
+        $socios = $entityManager->getRepository(Socio::class)->findAll();
+
+        return $this->render('socio/list.html.twig', [
+            'socios' => $socios,
         ]);
     }
 }
--- backend/src/Form/SocioType.php
+++ backend/src/Form/SocioType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Socio;
+use App\Entity\Empresa;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,13 +15,27 @@ class SocioType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
-        ;
+            ->add('nome', TextType::class, [
+                'label' => 'Nome'
+            ])
+            ->add('cpf', TextType::class, [
+                'label' => 'CPF'
+            ])
+            ->add('telefone', TextType::class, [
+                'label' => 'Telefone',
+                'required' => false
+            ])
+            ->add('empresa', EntityType::class, [
+                'class' => Empresa::class,
+                'choice_label' => 'nome',
+                'label' => 'Empresa'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'data_class' => Socio::class,
         ]);
     }
 }
